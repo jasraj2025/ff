ff9fa6793039a0c4867f09b09e04cd7a0bb26acf
jasraj2025 <80264078+jasraj2025@users.noreply.github.com>
2021-08-18 19:04:51 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBI07sRKeHbOjg71OwZN2fIlCkxMZWtmhY",
+    authDomain: "kwiter-project-d795d.firebaseapp.com",
+    databaseURL: "https://kwiter-project-d795d-default-rtdb.firebaseio.com",
+    projectId: "kwiter-project-d795d",
+    storageBucket: "kwiter-project-d795d.appspot.com",
+    messagingSenderId: "392775638266",
+    appId: "1:392775638266:web:c6ea0169394e2916b2fd43"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
